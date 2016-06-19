816f62c7874847c419e4374b9de78df36c7aad9f
piyo56 <piyo56.net@gmail.com>
2016-06-19 21:24:47 +0900

    Update jQuery_test.js

--- jQuery_test.js
+++ jQuery_test.js
@@ -1,12 +1,17 @@
+var count = 1;
+
 //読み込み完了時に実行する関数を指定
 $(loaded);
 
 function loaded(){
-  //ボタンタグをクリックしたときの動作を指定
-  $("button").click(change_text);
+    //ボタンタグをクリックしたときの動作を指定
+    $("button").click(change_text);
 }
 
 function change_text(){
-  //IDがmessageの要素のテキストを書き換え
-  $("#message").text("こんばんは");
+    //IDがmessageの要素のテキストを書き換え
+    $("#message").text("こんばんは");
+    //bodyにcountの内容を表示するパラグラフを追加
+    $("body").append("<p>" + count + "</p>");
+    count += 1;
 }
