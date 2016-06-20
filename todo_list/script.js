ef67d4352e883737c6f1d657efcaf3cc5e38eae3
piyo56 <piyo56.net@gmail.com>
2016-06-20 23:27:05 +0900

    Add a feature that done(delete) your task

--- todo_list/script.js
+++ todo_list/script.js
@@ -5,6 +5,9 @@ $(loaded);
 function loaded(){
     init();
     $("#add_button").click(addTask);
+    $(".done_button").click(function(){
+        deleteTask($(this).attr("id"));
+    });
 };
 
 function init(){
@@ -21,8 +24,6 @@ function printTasks(){
     var target_div = "#output";
     var task_div = "";
 
-    console.log(task_div);
-
     //一度divを空にしてからtodoリスト(tasks)の中身を表示
     $(target_div).html("");
     for(var i=0; i<tasks.length; i++) {
@@ -34,7 +35,6 @@ function printTasks(){
                            'done' +
                        '</div>' +
                    '</div>';
-        console.log(tasks[i]);
         $(target_div).append(task_div);
     }
 }
@@ -54,3 +54,20 @@ function addTask(){
     }
 }
 
+function deleteTask(task_id){
+    console.log("delete");
+    //押下されたdoneボタンのidから番号（添字）を取得
+    var index = Number(task_id.split("task").join(""));
+
+    //tasks[index]を削除してローカルストレージを更新
+    tasks.splice(index, 1);
+    localStorage.setItem(save_key, JSON.stringify(tasks));
+
+    //表示
+    printTasks();
+    
+    //TODO
+    //addは自然に連続で実行できるのに，deleteは
+    //最後にloaded関数を呼んでおかないと連続で実行できない
+    $(loaded);
+}
